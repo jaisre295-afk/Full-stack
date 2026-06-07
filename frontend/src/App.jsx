b248b032c3e8bac196d643d83cf9df70d0611f27
jaisre <jaisre295@gmail.com>
2026-06-07 15:48:09 +0530

    useEffect and react-router-dom completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,27 @@
 import Parent from './Pages/Parent'
 import Home from './Components/Home'
 import About from './Components/About'
-import Contact from './Components/Contact'
+import Contact from './ComponentS/Contact'
 import Service from './Components/Service'
-import Navbar from './pages/Navbar'
+import {Routes,Route} from 'react-router-dom'
+import Navbar from "./pages/Navbar"
 import Counter from './pages/Counter'
+import Effect from './pages/Effect'
 
 const App = () => {
   return (
     //Fragment tag
     <>
     <Navbar/>
-    <h1>Hello World!!</h1>
-    <h1>Hello World!!</h1>
-    <Parent/>
-    <Home/>
-    <About/>
-    <Contact/>
-    <Service/>
-    <Counter/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/contact' element={<Contact/>}></Route>
+      <Route path='/service' element={<Service/>}></Route>
+      <Route path='/parent' element={<Parent/>}></Route>
+      <Route path='/useState' element={<Counter/>}></Route>
+      <Route path='/useEffect' element={<Effect/>}></Route>
+    </Routes>
     </>
   )
 }
